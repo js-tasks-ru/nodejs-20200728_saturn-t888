cd8cf005414d1625270921658b34d97010899e5f
Vazha <Saturn.T888@gmail.com>
2020-07-28 17:47:56 +0300

    Solution2 01-into/01-sum

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -3,15 +3,10 @@ function sum (a, b) {
 		console.log ('Two numbers required!');
 		return;
 	}
-	try {
-		if ( (typeof (a) !== 'number' || isNaN (a)) || (typeof (b) !== 'number' || isNaN (b)) ) {
-			throw new TypeError;
-		}
-	} catch (err) {
-		console.log (err.name);
-	}
-
-	return (a + b)	;
+	
+	if ( (typeof (a) !== 'number' || isNaN (a)) || (typeof (b) !== 'number' || isNaN (b)) ) throw new TypeError;
+	
+	return (a + b);
 }
 
 module.exports = sum;
