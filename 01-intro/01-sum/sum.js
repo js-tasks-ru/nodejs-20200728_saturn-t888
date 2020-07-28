04c8510fbbec514e0be0372dea2b0ea12f0a5e4a
Vazha <Saturn.T888@gmail.com>
2020-07-28 17:12:26 +0300

    Solution 01-into/01-sum

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,17 @@
-function sum(a, b) {
-  /* ваш код */
+function sum (a, b) {
+	if (!a || !b) {
+		console.log ('Two numbers required!');
+		return;
+	}
+	try {
+		if ( (typeof (a) !== 'number' || isNaN (a)) || (typeof (b) !== 'number' || isNaN (b)) ) {
+			throw new TypeError;
+		}
+	} catch (err) {
+		console.log (err.name);
+	}
+
+	return (a + b)	;
 }
 
 module.exports = sum;
