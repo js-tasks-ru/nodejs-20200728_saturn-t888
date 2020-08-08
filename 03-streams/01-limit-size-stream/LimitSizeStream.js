17e43a55739fe2e0a5f3de95003d4bbf728fda5a
Vazha <Saturn.T888@gmail.com>
2020-08-08 18:59:22 +0300

    03-streams 01-limit-size-stream

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,9 +4,21 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.myLimit = options.limit || 0;
+    this.curLimit = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    let stringSize = parseInt (unescape(encodeURIComponent( chunk.toString ('utf8') ).length));
+    
+    this.curLimit += stringSize;
+
+    if ( this.curLimit > this.myLimit ) {
+      callback (new LimitExceededError ( ));
+      return;
+    }
+        
+    callback (null, chunk);
 
   }
 }
