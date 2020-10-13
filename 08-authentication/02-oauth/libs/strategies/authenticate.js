f5326aad8fbb8bb99fcab3c91c55c0ccb6b9efc9
Vazha <Saturn.T888@gmail.com>
2020-10-13 22:10:23 +0300

    Solution 08-02 und 08-03

--- 08-authentication/02-oauth/libs/strategies/authenticate.js
+++ 08-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,19 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const { ContextReplacementPlugin } = require('webpack');
+const User = require ('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  if (!email) {
+    return done (null, false, 'Не указан email');
+  }
+  try {
+    const user = await User.findOne ({email: email});
+    if (!user) {
+      const newUser = await User.create ({email, displayName});
+      done (null, newUser);
+    } else {
+      done (null, user);
+    }
+  } catch (err) {
+    done (err, false);
+  }
 };
