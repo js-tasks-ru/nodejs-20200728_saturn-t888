757a8eda344a2e858072e15942ebd33c19faaae6
Vazha <Saturn.T888@gmail.com>
2020-10-21 00:41:10 +0300

    09-01 and 09-02 + Fix 08-03

--- 09-email/01-registration/controllers/registration.js
+++ 09-email/01-registration/controllers/registration.js
@@ -4,8 +4,72 @@ const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
 
+    try {
+    
+        const reqName = ctx.request.body.displayName,
+            reqEmail = ctx.request.body.email,
+            reqPassword = ctx.request.body.password;
+
+        const user = await User.findOne ({email: reqEmail});
+
+        if (!user) {
+            // creating User //
+            const verificationToken = uuid ();
+            const newUser = new User ({email: reqEmail,
+                                    displayName: reqName,
+                                    verificationToken: verificationToken});
+            await newUser.setPassword (reqPassword);
+            await newUser.save ();
+
+            // sending Email //
+            await sendMail ({
+                template: 'confirmation',
+                locals: {token: 'token'},
+                to: reqEmail,
+                subject: 'Подтвердите почту'
+            });
+
+            // sending Response //
+            ctx.status = 200;
+            ctx.body = {status: 'ok'};
+
+        } else {
+            ctx.status = 400;
+            ctx.body = {errors: {email: 'Такой email уже существует'}};
+        }
+    } catch (err) {
+        ctx.throw (500, err.message);
+    }
+
 };
 
 module.exports.confirm = async (ctx, next) => {
+
+    try {
+
+        reqToken = ctx.request.body.verificationToken;
+
+        const user = await User.findOne ({verificationToken: reqToken}) || null;
+
+        if (!user) {
+            ctx.throw (400, 'Ссылка подтверждения недействительна или устарела');
+        } else {
+            // deleting Token //
+            user.verificationToken = undefined;
+            user.save ();
+
+            // generating sessionToken //
+            const sessionToken = uuid ();
+
+            // sending Response //
+            ctx.status = 200;
+            ctx.body = {token: sessionToken};
+            
+            return;
+        }
+
+    } catch (err) {
+        ctx.throw (500, err.message);
+    }
   
 };
--- 09-email/02-order/controllers/orders.js
+++ 09-email/02-order/controllers/orders.js
@@ -1,8 +1,47 @@
 const Order = require('../models/Order');
+const User = require ('../models/User');
+const Product = require ('../models/Product');
+const Session = require ('../models/Session');
 const sendMail = require('../libs/sendMail');
+const mustBeAuthenticated = require('../libs/mustBeAuthenticated');
 
 module.exports.checkout = async function checkout(ctx, next) {
+  
+    await checkAuth (ctx, next);
+
+    const product = await Product.findById (ctx.request.body.product) || null;
+    
+    const order = await Order.create ({
+        user: !!ctx.user._id ? ctx.user._id : null,
+        product: !!product ? product._id : null,
+        phone: !!ctx.request.body.phone ? ctx.request.body.phone : null,
+        address: !!ctx.request.body.address ? ctx.request.body.address : null
+    });
+
+    if (!!order) {
+        ctx.status = 200;
+        ctx.body = {order: order._id};
+
+        // sending Email //
+        await sendMail ({
+            template: 'order-confirmation',
+            locals: {id: order._id, product: product.title},
+            to: ctx.user.email,
+            subject: 'Ваш заказ сформирован'
+        });
+    }
+
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+
+    await checkAuth (ctx, next);
+
+    const orders = await Order.find ({user: ctx.user._id});
+    ctx.body = {orders: orders};
 };
+
+/* Authentication Check */
+async function checkAuth (ctx, next) {
+    mustBeAuthenticated (ctx, next);
+}
