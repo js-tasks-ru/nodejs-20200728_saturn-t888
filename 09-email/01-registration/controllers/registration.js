d4d804f6cf8a8a061d73a4d2d1d68acd975c59e1
Vazha <Saturn.T888@gmail.com>
2020-10-21 21:19:10 +0300

    09-01 Refactoring

--- 09-email/01-registration/controllers/registration.js
+++ 09-email/01-registration/controllers/registration.js
@@ -3,73 +3,60 @@ const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
-
-    try {
     
-        const reqName = ctx.request.body.displayName,
-            reqEmail = ctx.request.body.email,
-            reqPassword = ctx.request.body.password;
+    const reqName = ctx.request.body.displayName,
+        reqEmail = ctx.request.body.email,
+        reqPassword = ctx.request.body.password;
 
-        const user = await User.findOne ({email: reqEmail});
+    const user = await User.findOne ({email: reqEmail});
 
-        if (!user) {
-            // creating User //
-            const verificationToken = uuid ();
-            const newUser = new User ({email: reqEmail,
-                                    displayName: reqName,
-                                    verificationToken: verificationToken});
-            await newUser.setPassword (reqPassword);
-            await newUser.save ();
+    if (!user) {
+        // creating User //
+        const verificationToken = uuid ();
+        const newUser = new User ({email: reqEmail,
+                                displayName: reqName,
+                                verificationToken: verificationToken});
+        await newUser.setPassword (reqPassword);
+        await newUser.save ();
 
-            // sending Email //
-            await sendMail ({
-                template: 'confirmation',
-                locals: {token: 'token'},
-                to: reqEmail,
-                subject: 'Подтвердите почту'
-            });
+        // sending Email //
+        await sendMail ({
+            template: 'confirmation',
+            locals: {token: 'token'},
+            to: reqEmail,
+            subject: 'Подтвердите почту'
+        });
 
-            // sending Response //
-            ctx.status = 200;
-            ctx.body = {status: 'ok'};
+        // sending Response //
+        ctx.status = 200;
+        ctx.body = {status: 'ok'};
 
-        } else {
-            ctx.status = 400;
-            ctx.body = {errors: {email: 'Такой email уже существует'}};
-        }
-    } catch (err) {
-        ctx.throw (500, err.message);
+    } else {
+        ctx.status = 400;
+        ctx.body = {errors: {email: 'Такой email уже существует'}};
     }
-
 };
 
 module.exports.confirm = async (ctx, next) => {
 
-    try {
+    const reqToken = ctx.request.body.verificationToken;
 
-        reqToken = ctx.request.body.verificationToken;
+    const user = await User.findOne ({verificationToken: reqToken}) || null;
 
-        const user = await User.findOne ({verificationToken: reqToken}) || null;
+    if (!user) {
+        ctx.throw (400, 'Ссылка подтверждения недействительна или устарела');
+    } else {
+        // deleting Token //
+        user.verificationToken = undefined;
+        user.save ();
 
-        if (!user) {
-            ctx.throw (400, 'Ссылка подтверждения недействительна или устарела');
-        } else {
-            // deleting Token //
-            user.verificationToken = undefined;
-            user.save ();
+        // generating sessionToken //
+        const sessionToken = uuid ();
 
-            // generating sessionToken //
-            const sessionToken = uuid ();
-
-            // sending Response //
-            ctx.status = 200;
-            ctx.body = {token: sessionToken};
-            
-            return;
-        }
-
-    } catch (err) {
-        ctx.throw (500, err.message);
+        // sending Response //
+        ctx.status = 200;
+        ctx.body = {token: sessionToken};
+        
+        return;
     }
-  
 };
